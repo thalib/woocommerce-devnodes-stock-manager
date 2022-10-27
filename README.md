b0acd8f458affa45a03df6a2d06636995457794c
aSensar <admin@asensar.com>
2022-10-27 16:04:16 +0530

    Updated radme and url of docbook

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 Open Source Stock Manager for Woocommerce 
 
 Fast, Free and Open Source
+
+You don’t need to visit each product page to view and edit stock fields. Stock Manager plugin provides a dashboard to view all product stock and inventory information details at one place
+
+In Fast Stock Manager you can make edits to the following product stock data fields:
+
+* Title
+* MRP
+* Sale Price
+* Cost Price
+* Quantity
+* Weight
--- open-stock-manager.php
+++ open-stock-manager.php
@@ -1,16 +1,16 @@
 <?php
 /**
  * Plugin Name:       Fast Stock Manager
- * Plugin URI:        https://thalib.com/plugins/the-basics/
+ * Plugin URI:        https://github.com/thalib/woocommerce-faststock-manager
  * Description:       Open Source Stock Manager for Woocommerce
  * Version:           0.0.1
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Mohamed Thalib H
- * Author URI:        https://author.example.com/
+ * Author URI:        https://github.com/thalib
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Update URI:        https://example.com/my-plugin/
+ * Update URI:        https://github.com/thalib/woocommerce-faststock-manager
  * Text Domain:       faststock
  * Domain Path:       /languages
  */
